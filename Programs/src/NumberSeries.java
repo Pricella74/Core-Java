baa03e2bfea8de8077b38cd27bd3c603a52214ac
Pricella <pricellamosess74@gmail.com>
2022-08-18 18:33:10 +0530

    secondcommit

---
+++ Programs/src/NumberSeries.java
@@ -0,0 +1,12 @@
+
+public class NumberSeries {
+   public static void main(String args[]) {
+      int i = 1;
+      System.out.println("Using do while loop...");
+      do {
+         System.out.print(i + " ");
+         i++;
+      }
+      while(i <= 10);
+   }
+}
---
+++ Programs/src/ty/com/NumberSeries.java
@@ -0,0 +1,13 @@
+package ty.com;
+
+public class NumberSeries {
+   public static void main(String args[]) {
+      int i = 1;
+      System.out.println("Using do while loop...");
+      do {
+         System.out.print(i + " ");
+         i++;
+      }
+      while(i <= 10);
+   }
+}
